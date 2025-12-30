87d33090a9d091985db74969a31303c9602c2e61
Kinshuk Pandala <152378610+kinshukpandala@users.noreply.github.com>
2025-12-30 10:21:29 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+JWT_SECRET = os.getenv("JWT_SECRET")
+REDIRECT_URI = "http://localhost:8000/auth/callback"
